7ebeeb7e42e42ef130b507ceb31410d4fbe0e839
Bryce B <bryce.boddy@gmail.com>
2020-06-23 07:26:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,8 +9,8 @@ class Song < ActiveRecord::Base
   def drake_made_this
     drake = Artist.find_or_create_by(name: 'Drake')
     self.artist = drake
-    all_artist = Artist.all.map {|a| a.name}
-    if all_artist.include? "Drake"
+    #all_artist = Artist.all.map {|a| a.name}
+    #if all_artist.include? "Drake"
       #return true
     #elsif
       #drake =  Artist.create(name: "Drake")
